27623e7f18b8c9cd37413ca2642af131fa7226da
Kiran Mondal <kiranmondal5516@gmail.com>
2026-06-23 22:12:30 +0530

    Refactored code formatting in `om-core/opcodes.py` via Telegram Bot Sandbox Engine

--- om-core/opcodes.py
+++ om-core/opcodes.py
@@ -10,5 +10,13 @@ class OpCode:
     MUL        = 0x06
     DIV        = 0x07
     PRINT      = 0x08
-    HALT       = 0x00
+    INPUT      = 0x09
     
+    # NEW: Logic and Control Flow Instructions
+    CMP_EQ     = 0x0A   # Compare Equals (==)
+    CMP_LT     = 0x0B   # Compare Less Than (<)
+    CMP_GT     = 0x0C   # Compare Greater Than (>)
+    JUMP_FALSE = 0x0D   # Jump if condition is false (Used for IF / WHILE)
+    JUMP       = 0x0E   # Unconditional Jump (Used to loop back)
+    
+    HALT       = 0x00
